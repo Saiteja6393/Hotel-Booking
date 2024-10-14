60c7cc8141ed14b63acfd201179b756a9b981fe2
sai teja <94728002+Saiteja6393@users.noreply.github.com>
2024-10-14 19:23:01 +0530

    Update models.py

--- src/models.py
+++ src/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Hotels(models.Model):
     #h_id,h_name,owner ,location,rooms
-    name = models.CharField(max_length=30,default="krishna")
+    name = models.CharField(max_length=30)
     owner = models.CharField(max_length=20)
     location = models.CharField(max_length=50)
-    state = models.CharField(max_length=50,default="maharashtra")
-    country = models.CharField(max_length=50,default="india")
+    state = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
     def __str__(self):
         return self.name
 
